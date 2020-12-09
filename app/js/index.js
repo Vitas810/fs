a9ebfa28bfe9548d34c0180cdce8bfff37f128f8
Vitaly P <frontendnsk@gmail.com>
2020-12-10 02:34:18 +0700

    создание структуры файлов

---
+++ app/js/index.js
@@ -0,0 +1,11 @@
+const cat = document.querySelector('.catalog-top');
+const button = document.querySelector('.button');
+const line = document.querySelector('.line');
+const breadCrumbs = document.querySelector('.bread-crumbs');
+
+button.addEventListener('click', (el) => {
+  el.preventDefault();
+  cat.classList.toggle('visible');
+  line.classList.toggle('novisible');
+  breadCrumbs.classList.toggle('novisible');
+});
