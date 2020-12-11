2f611c992102cb45189f9027b568cb513975472b
Vitas810 <59773453+Vitas810@users.noreply.github.com>
2020-12-12 00:16:05 +0700

    исправление скрипта

--- app/js/index.js
+++ app/js/index.js
@@ -1,11 +1,23 @@
-const cat = document.querySelector('.catalog-top');
-const button = document.querySelector('.button');
-const line = document.querySelector('.line');
-const breadCrumbs = document.querySelector('.bread-crumbs');
+const generColor = () => {
+  let color = '#'+((1<<24)*Math.random()|0).toString(16);
+  return color;
+}
 
-button.addEventListener('click', (el) => {
-  el.preventDefault();
-  cat.classList.toggle('visible');
-  line.classList.toggle('novisible');
-  breadCrumbs.classList.toggle('novisible');
+const genElem =() => {
+  let randElem = document.querySelectorAll("*");
+  let counter = Math.floor(Math.random() * randElem.length);
+  let originalStyle = getComputedStyle(randElem[counter]);
+  originalStyle = originalStyle.backgroundColor;
+  randElem[counter].style.backgroundColor = generColor();
+
+  setTimeout(el =>{
+    randElem[counter].style.backgroundColor = originalStyle;
+    console.log();
+  },4000)  
+}
+ document.addEventListener("DOMContentLoaded", function(event) {
+    setInterval(el => {
+      genElem();
+    },2000)
 });
+
