de9f9b8e506b31b4eb2892942d857f80663e31bc
Vitas810 <59773453+Vitas810@users.noreply.github.com>
2020-12-13 00:06:14 +0700

    Создание  карточек

--- app/js/index.js
+++ app/js/index.js
@@ -10,3 +10,114 @@ button.addEventListener('click', (el) => {
   breadCrumbs.classList.toggle('novisible');
 });
 
+
+const setItems = {
+  allItem: [
+    {
+      title: 'Айну',
+      price: 12000,
+      src: 'assets/image.jpeg',
+      alt: 'image',
+      button: 'В корзину'
+    },
+    {
+      title: 'Афганская борзая',
+      price: 15000,
+      src: 'assets/image1.jpeg',
+      alt: 'image',
+      button: 'В корзину'
+    },
+    {
+      title: 'Барбет',
+      price: 15000,
+      src: 'assets/image2.jpeg',
+      alt: 'image',
+      button: 'В корзину'
+    },
+    {
+      title: 'Бассет',
+      price: 10000,
+      src: 'assets/image3.jpeg',
+      alt: 'image',
+      button: 'В корзину'
+    },
+    {
+      title: 'Легавой',
+      price: 39000,
+      src: 'assets/image4.jpeg',
+      alt: 'image',
+      button: 'В корзину'
+    },
+    {
+      title: 'Веттерхун',
+      price: 12000,
+      src: 'assets/image5.jpeg',
+      alt: 'image',
+      button: 'В корзину'
+    },
+    {
+      title: 'Древера',
+      price: 17000,
+      src: 'assets/image6.jpeg',
+      alt: 'image',
+      button: 'В корзину'
+    },
+    {
+      title: 'Ирландский терьер',
+      price: 15000,
+      src: 'assets/image7.jpeg',
+      alt: 'image',
+      button: 'В корзину'
+    },
+    {
+      title: 'Амереканский кокер',
+      price: 10000,
+      src: 'assets/image9.jpeg',
+      alt: 'image',
+      button: 'В корзину'
+    },
+    {
+      title: 'Английский Кокер',
+      price: 8000,
+      src: 'assets/image10.jpeg',
+      alt: 'image',
+      button: 'В корзину'
+    },
+    {
+      title: 'Дункер',
+      price: 20000,
+      src: 'assets/image11.jpeg',
+      alt: 'image',
+      button: 'В корзину'
+    },
+    {
+      title: 'Спаниель',
+      price: 5000,
+      src: 'assets/image12.jpeg',
+      alt: 'image',
+      button: 'В корзину'
+    }
+  ]
+}
+
+let randomItems = setItems.allItem.sort(() => Math.random() - 0.5);
+
+function createItems() {
+  let cardFragment = document.createDocumentFragment();
+  let temlateItems = document.querySelector('#cardProduct');
+  randomItems.forEach( element => {
+    const mainItem = temlateItems.content.cloneNode(true)
+    mainItem.querySelector('img').src = element.src;
+    mainItem.querySelector('img').alt = element.alt;
+    mainItem.querySelector('h3').textContent =element.title;
+    mainItem.querySelector('.main-items__price').textContent =element.price;
+    mainItem.querySelector('.main-items__button').textContent =element.button;
+    cardFragment.appendChild(mainItem);
+  });
+  let list  = document.querySelector('.main-items');
+  list.appendChild(cardFragment);
+
+}
+
+createItems();
+
