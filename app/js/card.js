5e61e3b4cef575ff43c9522afcd9675c812cbf06
Vitas810 <59773453+Vitas810@users.noreply.github.com>
2020-12-19 23:33:46 +0700

    создание генерации ссылок, исправление карточек  в .json

--- app/js/card.js
+++ app/js/card.js
@@ -1,275 +1,14 @@
-// const cardItems = [
-//   {
-//     id: 1,
-//     title: 'Характеристика Айну',
-//     typeName: 'тип',
-//     sizeName: 'Размер',
-//     mainName: 'Основные черты',
-//     frequentName: 'Частые заболевания',
-//     intelligenceName: 'Интеллект',
-//     priceName: 'Стоимость',
-//     cardTypes: [1, 2],
-//     size: 15,
-//     kg: 'кг',
-//     cardFeatures: [5, 6, 7, 8],
-//     cardFrequentIllnesses: [1, 2],
-//     Intelligence: '45%',
-//     btnTake: 'Забрать',
-//     btnAddCard: 'Добавить в корзину',
-//     priceCard: 12000,
-//     p: 'P',
-//     srcCard: 'assets/image.jpeg',
-//     alt: 'image',
-//   },
-//   /*   {
-//     id: 2,
-//     title: 'Характеристика Афганская борзая',
-//     typeName: 'тип',
-//     sizeName: 'Размер',
-//     mainName: 'Основные черты',
-//     frequentName: 'Частые заболевания',
-//     intelligenceName: 'Интеллект',
-//     priceName: 'Стоимость',
-//     cardTypes: [1, 2],
-//     size: 18,
-//     kg: 'кг',
-//     cardFeatures: [5, 6, 1, 2],
-//     cardFrequentIllnesses: [1, 2],
-//     Intelligence: '45%',
-//     btnTake: 'Забрать',
-//     btnAddCard: 'Добавить в корзину',
-//     priceCard: 15000,
-//     p: 'P',
-//     srcCard: 'assets/image1.jpeg',
-//     alt: 'image',
-//   },
-//   {
-//     id: 3,
-//     title: 'Характеристика Барбет',
-//     typeName: 'тип',
-//     sizeName: 'Размер',
-//     mainName: 'Основные черты',
-//     frequentName: 'Частые заболевания',
-//     intelligenceName: 'Интеллект',
-//     priceName: 'Стоимость',
-//     cardTypes: [1, 2],
-//     size: 10,
-//     kg: 'кг',
-//     cardFeatures: [5, 6, 1, 2],
-//     cardFrequentIllnesses: [1, 2],
-//     Intelligence: '45%',
-//     btnTake: 'Забрать',
-//     btnAddCard: 'Добавить в корзину',
-//     priceCard: 15000,
-//     p: 'P',
-//     srcCard: 'assets/image2.jpeg',
-//     alt: 'image',
-//   },
-//   {
-//     id: 4,
-//     title: 'Характеристика Бассет',
-//     typeName: 'тип',
-//     sizeName: 'Размер',
-//     mainName: 'Основные черты',
-//     frequentName: 'Частые заболевания',
-//     intelligenceName: 'Интеллект',
-//     priceName: 'Стоимость',
-//     cardTypes: [1, 2],
-//     size: 12,
-//     kg: 'кг',
-//     cardFeatures: [5, 6, 1, 2],
-//     cardFrequentIllnesses: [1, 2],
-//     Intelligence: '45%',
-//     btnTake: 'Забрать',
-//     btnAddCard: 'Добавить в корзину',
-//     priceCard: 10000,
-//     p: 'P',
-//     srcCard: 'assets/image3.jpeg',
-//     alt: 'image',
-//   },
-//   {
-//     id: 5,
-//     title: 'Характеристика Легавой',
-//     typeName: 'тип',
-//     sizeName: 'Размер',
-//     mainName: 'Основные черты',
-//     frequentName: 'Частые заболевания',
-//     intelligenceName: 'Интеллект',
-//     priceName: 'Стоимость',
-//     cardTypes: [1, 2],
-//     size: 14,
-//     kg: 'кг',
-//     cardFeatures: [5, 6, 1, 2],
-//     cardFrequentIllnesses: [1, 2],
-//     Intelligence: '45%',
-//     btnTake: 'Забрать',
-//     btnAddCard: 'Добавить в корзину',
-//     priceCard: 39000,
-//     p: 'P',
-//     srcCard: 'assets/image4.jpeg',
-//     alt: 'image',
-//   },
-//   {
-//     id: 6,
-//     title: 'Характеристика Веттерхун',
-//     typeName: 'тип',
-//     sizeName: 'Размер',
-//     mainName: 'Основные черты',
-//     frequentName: 'Частые заболевания',
-//     intelligenceName: 'Интеллект',
-//     priceName: 'Стоимость',
-//     cardTypes: [1, 2],
-//     size: 16,
-//     kg: 'кг',
-//     cardFeatures: [5, 6, 1, 2],
-//     cardFrequentIllnesses: [1, 2],
-//     Intelligence: '45%',
-//     btnTake: 'Забрать',
-//     btnAddCard: 'Добавить в корзину',
-//     priceCard: 12000,
-//     p: 'P',
-//     srcCard: 'assets/image5.jpeg',
-//     alt: 'image',
-//   },
-//   {
-//     id: 7,
-//     title: 'Характеристика Древера',
-//     typeName: 'тип',
-//     sizeName: 'Размер',
-//     mainName: 'Основные черты',
-//     frequentName: 'Частые заболевания',
-//     intelligenceName: 'Интеллект',
-//     priceName: 'Стоимость',
-//     cardTypes: [1, 2],
-//     size: 16,
-//     kg: 'кг',
-//     cardFeatures: [5, 6, 1, 2],
-//     cardFrequentIllnesses: [1, 2],
-//     Intelligence: '45%',
-//     btnTake: 'Забрать',
-//     btnAddCard: 'Добавить в корзину',
-//     priceCard: 17000,
-//     p: 'P',
-//     srcCard: 'assets/image6.jpeg',
-//     alt: 'image',
-//   },
-//   {
-//     id: 8,
-//     title: 'Характеристика Ирландский терьер',
-//     typeName: 'тип',
-//     sizeName: 'Размер',
-//     mainName: 'Основные черты',
-//     frequentName: 'Частые заболевания',
-//     intelligenceName: 'Интеллект',
-//     priceName: 'Стоимость',
-//     cardTypes: [1, 2],
-//     size: 16,
-//     kg: 'кг',
-//     cardFeatures: [5, 6, 1, 2],
-//     cardFrequentIllnesses: [1, 2],
-//     Intelligence: '45%',
-//     btnTake: 'Забрать',
-//     btnAddCard: 'Добавить в корзину',
-//     priceCard: 15000,
-//     p: 'P',
-//     srcCard: 'assets/image7.jpeg',
-//     alt: 'image',
-//   },
-//   {
-//     id: 9,
-//     title: 'Характеристика Амереканский кокер',
-//     typeName: 'тип',
-//     sizeName: 'Размер',
-//     mainName: 'Основные черты',
-//     frequentName: 'Частые заболевания',
-//     intelligenceName: 'Интеллект',
-//     priceName: 'Стоимость',
-//     cardTypes: [1, 2],
-//     size: 16,
-//     kg: 'кг',
-//     cardFeatures: [5, 6, 1, 2],
-//     cardFrequentIllnesses: [1, 2],
-//     Intelligence: '45%',
-//     btnTake: 'Забрать',
-//     btnAddCard: 'Добавить в корзину',
-//     priceCard: 10000,
-//     p: 'P',
-//     srcCard: 'assets/image9.jpeg',
-//     alt: 'image',
-//   },
-//   {
-//     id: 10,
-//     title: 'Характеристика Английский Кокер',
-//     typeName: 'тип',
-//     sizeName: 'Размер',
-//     mainName: 'Основные черты',
-//     frequentName: 'Частые заболевания',
-//     intelligenceName: 'Интеллект',
-//     priceName: 'Стоимость',
-//     cardTypes: [1, 2],
-//     size: 16,
-//     kg: 'кг',
-//     cardFeatures: [5, 6, 1, 2],
-//     cardFrequentIllnesses: [1, 2],
-//     Intelligence: '45%',
-//     btnTake: 'Забрать',
-//     btnAddCard: 'Добавить в корзину',
-//     priceCard: 8000,
-//     p: 'P',
-//     srcCard: 'assets/image10.jpeg',
-//     alt: 'image',
-//   },
-//   {
-//     id: 11,
-//     title: 'Характеристика Дункер',
-//     typeName: 'тип',
-//     sizeName: 'Размер',
-//     mainName: 'Основные черты',
-//     frequentName: 'Частые заболевания',
-//     intelligenceName: 'Интеллект',
-//     priceName: 'Стоимость',
-//     cardTypes: [1, 2],
-//     size: 16,
-//     kg: 'кг',
-//     cardFeatures: [5, 6, 1, 2],
-//     cardFrequentIllnesses: [1, 2],
-//     Intelligence: '45%',
-//     btnTake: 'Забрать',
-//     btnAddCard: 'Добавить в корзину',
-//     priceCard: 20000,
-//     p: 'P',
-//     srcCard: 'assets/image11.jpeg',
-//     alt: 'image',
-//   },
-//   {
-//     id: 12,
-//     title: 'Характеристика Спаниель',
-//     typeName: 'тип',
-//     sizeName: 'Размер',
-//     mainName: 'Основные черты',
-//     frequentName: 'Частые заболевания',
-//     intelligenceName: 'Интеллект',
-//     priceName: 'Стоимость',
-//     cardTypes: [1, 2],
-//     size: 16,
-//     kg: 'кг',
-//     cardFeatures: [5, 6, 1, 2],
-//     cardFrequentIllnesses: [1, 2],
-//     Intelligence: '45%',
-//     btnTake: 'Забрать',
-//     btnAddCard: 'Добавить в корзину',
-//     priceCard: 5000,
-//     p: 'P',
-//     srcCard: 'assets/image12.jpeg',
-//     alt: 'image',
-//   }, */
-// ];
-
 let xmlHTTP = new XMLHttpRequest();
 xmlHTTP.onload = function (ev) {
-  //console.log(ev.target.responseText);
   let cardItems = JSON.parse(ev.target.responseText);
 
+  function getUrlId(){
+      let urlId = window.location.search; 
+      let arrayId = []; 
+      arrayId = (urlId.substring(3));  
+      arrayId = parseInt(arrayId, 10)
+      return arrayId;
+  }
   const cardTypesList = [
     { id: 1, label: 'Охотничьи' },
     { id: 2, label: 'Компаньоны' },
@@ -277,78 +16,71 @@ xmlHTTP.onload = function (ev) {
     { id: 4, label: 'Служебные' },
   ];
   const cardFeaturesList = [
-    { id: 1, label: 'Отсутствует чувство страха' },
-    { id: 2, label: 'Мало линяет' },
-    { id: 3, label: 'Отличное здоровье' },
-    { id: 4, label: 'Хорошее послушание' },
-    { id: 5, label: 'Очень преданная' },
-    { id: 6, label: 'Дружелюбная' },
-    { id: 7, label: 'Подходит для охоты' },
-    { id: 8, label: 'Мало лает' },
+    { id: 5, label: 'Отсутствует чувство страха' },
+    { id: 6, label: 'Мало линяет' },
+    { id: 7, label: 'Отличное здоровье' },
+    { id: 8, label: 'Хорошее послушание' },
+    { id: 9, label: 'Очень преданная' },
+    { id: 10, label: 'Дружелюбная' },
+    { id: 11, label: 'Подходит для охоты' },
+    { id: 12, label: 'Мало лает' },
   ];
   const cardFrequentIList = [
-    { id: 1, label: 'Глухота ' },
-    { id: 2, label: 'Паралич гортани' },
+    { id: 13, label: 'Глухота ' },
+    { id: 14, label: 'Паралич гортани' },
   ];
 
   let arrayTypes = [];
   let arrayFeatures = [];
   let arrayFrequentIllnesses = [];
+  let searchId = getUrlId();
+  let cardElem = cardItems.find(city => city.id === searchId)
+  //console.log('cardElem',cardElem.cardTypes);
 
-  cardItems.forEach((element) => {
-    //console.log(Array.isArray(element.cardTypes));
-    element.cardTypes.forEach((item) => {
-      const types = cardTypesList.find((types) => types.id === item);
+  cardElem.cardTypes.forEach(id => {
+    const types = cardTypesList.find((types) => types.id === id);
       if (!types) return;
       arrayTypes.push(types.label);
-    });
-    element.cardFeatures.forEach((el) => {
-      const features = cardFeaturesList.find((features) => features.id === el);
+  });
+  
+  cardElem.cardFeatures.forEach(id => {
+    const features = cardFeaturesList.find((features) => features.id === id);
       if (!features) return;
-      arrayFeatures.push(features.label);
-
-      //console.log(features.label);
-    });
-    element.cardFrequentIllnesses.forEach((el) => {
-      const FrequentIllnesses = cardFrequentIList.find(
-        (FrequentIllnesses) => FrequentIllnesses.id === el
-      );
+         arrayFeatures.push(features.label);
+  });
+  cardElem.cardFrequentIllnesses.forEach(id => {
+    const FrequentIllnesses = cardFrequentIList.find((FrequentIllnesses) => FrequentIllnesses.id === id);
       if (!FrequentIllnesses) return;
-      arrayFrequentIllnesses.push(FrequentIllnesses.label);
-      //console.log(FrequentIllnesses.label);
-    });
+         arrayFrequentIllnesses.push(FrequentIllnesses.label);
   });
 
-  function createCards() {
+  function createCards(cardElem) {
     let cardFragment = document.createDocumentFragment();
     let temlateCard = document.querySelector('#cardElement');
-    cardItems.forEach((element) => {
-      const mainCard = temlateCard.content.cloneNode(true);
-      mainCard.querySelector('img').src = element.srcCard;
-      mainCard.querySelector('img').alt = element.alt;
-      mainCard.querySelector('.card-optins__title').textContent = element.title;
-      mainCard.querySelector('.types_title1').textContent = element.typeName;
-      mainCard.querySelector('.types_value1').textContent = arrayTypes;
-      mainCard.querySelector('.types_title2').textContent = element.sizeName;
-      mainCard.querySelector('.types_value2').textContent = element.size + element.kg;
-      mainCard.querySelector('.types_title3').textContent = element.mainName;
-      mainCard.querySelector('.types_value3').textContent = arrayFeatures;
-      mainCard.querySelector('.types_title4').textContent = element.frequentName;
-      mainCard.querySelector('.types_value4').textContent = arrayFrequentIllnesses;
-      mainCard.querySelector('.types_title5').textContent = element.intelligenceName;
-      mainCard.querySelector('.types_value5').textContent = element.Intelligence;
-      mainCard.querySelector('.types_title6').textContent = element.priceName;
-      mainCard.querySelector('.types_value6').textContent = element.priceCard + element.p;
-      mainCard.querySelector('.card-options_button').textContent = element.btnAddCard;
-      mainCard.querySelector('.btn-take').textContent = element.btnTake;
-      cardFragment.appendChild(mainCard);
-    });
+    const mainCard = temlateCard.content.cloneNode(true); 
+    mainCard.querySelector('img').src = cardElem.srcCard;
+    mainCard.querySelector('img').alt = cardElem.alt;
+    mainCard.querySelector('.card-optins__title').textContent = cardElem.title;
+    mainCard.querySelector('.types_title1').textContent = cardElem.typeName;
+    mainCard.querySelector('.types_value1').textContent = arrayTypes;
+    mainCard.querySelector('.types_title2').textContent = cardElem.sizeName;
+    mainCard.querySelector('.types_value2').textContent = cardElem.size + cardElem.kg;
+    mainCard.querySelector('.types_title3').textContent = cardElem.mainName;
+    mainCard.querySelector('.types_value3').textContent = arrayFeatures;
+    mainCard.querySelector('.types_title4').textContent = cardElem.frequentName;
+    mainCard.querySelector('.types_value4').textContent = arrayFrequentIllnesses;
+    mainCard.querySelector('.types_title5').textContent = cardElem.intelligenceName;
+    mainCard.querySelector('.types_value5').textContent = cardElem.Intelligence;
+    mainCard.querySelector('.types_title6').textContent = cardElem.priceName;
+    mainCard.querySelector('.types_value6').textContent = cardElem.priceCard + cardElem.p;
+    mainCard.querySelector('.card-options_button').textContent = cardElem.btnAddCard;
+    mainCard.querySelector('.btn-take').textContent = cardElem.btnTake;
+    cardFragment.appendChild(mainCard);
     let list = document.querySelector('.card');
     list.appendChild(cardFragment);
   }
-  createCards();
 
- 
+  createCards(cardElem);
 };
 xmlHTTP.open('GET', 'http://localhost:3000/api/card.json', true);
 xmlHTTP.send();
