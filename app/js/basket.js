7ec3f24c19be02e189db908985730677f32d9124
Vitas810 <59773453+Vitas810@users.noreply.github.com>
2020-12-25 00:16:40 +0700

    исправление подсчета суммы в корзине

--- app/js/basket.js
+++ app/js/basket.js
@@ -2,12 +2,12 @@ xmlHttp = new XMLHttpRequest();
 xmlHttp.onload = function (ev) {
 
   class CartBasketCount {
-    constructor (arrayitemBasket) {
-      this.arrayitemBasket = arrayitemBasket;
+    constructor () {
+
     }
 
     initBasket () {
-      let arrayitemBasket = [];
+      let arrayitemBasket = [];   
       let btnBasket = document.querySelectorAll('.main-items__button');
 
       for (let btnItem of btnBasket) {
@@ -19,17 +19,12 @@ xmlHttp.onload = function (ev) {
         let imageSrc = shopItem.querySelector('img').src;
         arrayitemBasket.push(price);
         addItemToCart(title, imageSrc,price)
-        //updateCartTotal()
-          
+        updateCartTotal()     
         })
       }
       function addItemToCart(title,imageSrc,price) {
         let basketCountlElement = document.querySelector('.cart_content');
-        let totalPtice = document.querySelector('.total-price');       
-        let totalSumm = arrayitemBasket.reduce((sum, arg) => parseInt(sum) + (parseInt(arg) || 0))
-        basketCountlElement.innerText = `Корзина (${arrayitemBasket.length})`;
-        totalPtice.innerText = `Сумма (${totalSumm})`;
-
+        let totalPtice = document.querySelector('.total-price');      
         let cartElement = 
             ` 
               <div class="basket-item__checkbox filter-checkbox-other">
@@ -42,23 +37,26 @@ xmlHttp.onload = function (ev) {
               <div class="basket-item__name">${title}</div>
               <div class="basket-item__buttons">
                 <button class="basket-item__button btn-minus">-</button>
-                <input type="number" class="basket-item__result" value="1" data-price="${price}" disabled>
+                <input type="number" class="basket-item__result" value="1" data-price="${price}" data-value="1" disabled>
                 <button class="basket-item__button btn-plus">+</button>
               </div>
-              <div class="basket-item__price">${price} Р</div>
+              <div class="basket-item__price">${price} </div>
               <button class="basket-btn-remove">
                 <svg class="icon_delete_basket">
                   <use xlink:href="assets/icons.svg#delete_basket"></use>
                 </svg>
               </button>
           `;
-            
+
         let addCartBasket = false;
         [...document.querySelectorAll('.basket-item__name')].forEach(item => {
           if(item.innerText === title) { return addCartBasket = true}
         })
 
         if(addCartBasket === false) {
+          let totalSumm = arrayitemBasket.reduce((sum, arg) => parseInt(sum) + (parseInt(arg) || 0))
+          basketCountlElement.innerText = `Корзина (${arrayitemBasket.length})`;
+          totalPtice.innerText = `Сумма (${totalSumm})`;
           let cartItem = document.createElement('li');
           cartItem.classList.add('basket-item') 
           cartItem.innerHTML = cartElement
@@ -66,76 +64,72 @@ xmlHttp.onload = function (ev) {
           list.append(cartItem);
           } else {
             alert('Товар уже добавлен в корзину');
+            return;
           }
       }
+    
+      const getItemSubTotalPrice = input => {
+        return Number(input.value) * Number(input.dataset.price);
+      }
+
+      const calculateSeparateItem = (basketItem, action) => {
+        const input = basketItem.querySelector('.basket-item__result'); 
+
+        switch (action) { 
+          case 'plus':
+            input.value++;
+            input.dataset.value = input.value;
+            updateCartTotal()
+            break;
+          case 'minus':
+            input.value--;        
+            break;
+        }
+        console.log(getItemSubTotalPrice(input));
+        basketItem.querySelector('.basket-item__price').textContent = getItemSubTotalPrice(input);
+      }
+      document.querySelector('.basket-items').addEventListener('click', event => {
+        if(event.target.classList.contains('btn-minus')) {
+          const input = event.target.closest('.basket-item').querySelector('.basket-item__result');
+          if (Number(input.value )!== 0) {
+            calculateSeparateItem(
+              event.target.closest('.basket-item'),
+              'minus'
+            );
+          }
+
+        }
+        if(event.target.classList.contains('btn-plus')) {
+          calculateSeparateItem(
+            event.target.closest('.basket-item'),
+            'plus'
+          );
+        }
+      });
+
+      function updateCartTotal() {
+        let cartItemContainer = document.getElementsByClassName('basket-items')[0]
+        let cartRows = cartItemContainer.getElementsByClassName('basket-item')
+        let total = 0
+        for (let i = 0; i < cartRows.length; i++) {
+            let quantity = 0
+            let cartRow = cartRows[i];
+            let quantityElement = cartRow.getElementsByClassName('basket-item__result')[0];
+            let price = quantityElement.dataset.price;
+            quantity = quantityElement.dataset.value;
+            console.log('price',price);
+            total = total + (price * quantity);
+           
+        }
+        total = Math.round(total * 100) / 100
+        document.getElementsByClassName('results_price')[0].innerText = `${total} P`
+      } 
     }
-  
+
   }
   let totalBsk = new CartBasketCount();
   totalBsk.initBasket();
-
-
-  const totalPriceWrapper = document.querySelector('.results_price');
-  const getItemSubTotalPrice = input => {
-    return Number(input.value) * Number(input.dataset.price);
-  }
-  const settotalprice = value => {
-    totalPriceWrapper.textContent = value;
-    totalPriceWrapper.dataset.value = value;
-  }
-
-  const init = () => {
-    let totalCost = 0;
-
-    [...document.querySelectorAll('.basket-item')].forEach(basketItem => {
-      totalCost += getItemSubTotalPrice(basketItem.querySelector('.basket-item__result'));
-    });
-    
-    settotalprice(totalCost);
-    
-  }
-
-  const calculateSeparateItem = (basketItem, action) => {
-    const input = basketItem.querySelector('.basket-item__result');
   
-
-    switch (action) { 
-      case 'plus':
-        input.value++;
-        settotalprice(Number(totalPriceWrapper.dataset.value) + Number(input.dataset.price));
-        break;
-      case 'minus':
-        input.value--;
-        //settotalprice(Number(totalPriceWrapper.dataset.value) - Number(input.dataset.price));
-        break;
-    }
-    basketItem.querySelector('.basket-item__price').textContent = getItemSubTotalPrice(input)
-  }
-
-  document.querySelector('.basket-items').addEventListener('click', event => {
-    if(event.target.classList.contains('btn-minus')) {
-      const input = event.target.closest('.basket-item').querySelector('.basket-item__result');
-      if (Number(input.value )!== 0) {
-        calculateSeparateItem(
-          event.target.closest('.basket-item'),
-          'minus'
-        );
-      }
-
-    }
-    if(event.target.classList.contains('btn-plus')) {
-       calculateSeparateItem(
-         event.target.closest('.basket-item'),
-         'plus'
-      );
-    }
-
-  });
-
-  init();
-
-
-
 };
 xmlHttp.open('GET', 'http://localhost:3000/api/catalog.json', true);
 xmlHttp.send();
